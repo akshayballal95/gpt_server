516d358bcbef2ae82618e0e5c0009abf6860287e
= <=>
2023-06-07 01:06:17 +0530

    cors

--- app/main.py
+++ app/main.py
@@ -12,7 +12,8 @@ origins = [
     "https://localhost.tiangolo.com",
     "http://localhost",
     "http://localhost:5173",
-    "https://www.akshaymakes.com"
+    "https://www.akshaymakes.com",
+    "https://akshaymakes.com"
 ]
 
 app.add_middleware(
